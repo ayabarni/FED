355b76ee3896cc01aee0ce3f329095e4ea619c86
ayabarni <aya.barni@hva.nl>
2024-09-26 13:57:27 +0200

    update 4

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
 console.log("hi");
+
+// open
+var openButton = document.querySelector("header div:nth-of-type(2) button:nth-of-type(2)");
+
+openButton.addEventListener("click",toggleMenu) ; 
+var deNav = document.querySelector("header nav");
+
+function toggleMenu() {  
+    
+   
+    
+    deNav.classList.toggle("toonMenu");
+}
+
