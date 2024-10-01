8db09292214cf37a498a001c72faf6524f662e4a
ayabarni <aya.barni@hva.nl>
2024-10-01 11:57:18 +0200

    update 6 voor het inleveren Co-authored-by: Sundous Kanaan <Sundous.kn@gmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,37 @@
 console.log("hi");
 
 // open
-var openButton = document.querySelector("header div:nth-of-type(2) button:nth-of-type(2)");
+var openButton = document.getElementById("fullSize-menuButton ");
+var deBody = document.getElementById('index')
 
 openButton.addEventListener("click",toggleMenu) ; 
 var deNav = document.querySelector("header nav");
 
 function toggleMenu() {  
-    
-   
-    
     deNav.classList.toggle("toonMenu");
+    deBody.classList.toggle("navOpen")
 }
 
+
+// news carousel
+
+var prevButton = document.querySelector('main article button:first-of-type');
+var nextButton = document.querySelector('main article button:last-of-type');
+var carousel = document.querySelector('main article ul');
+const scrollAmount = 500; // 500px 
+
+nextButton.addEventListener('click',goToNextElement );
+function goToNextElement(){
+        carousel.scrollBy({
+        left: scrollAmount,
+        behavior: 'smooth' // Zorgt voor vloeiend scrollen
+    });
+}
+
+prevButton.addEventListener('click',goToPrevElement );
+function goToPrevElement(){
+        carousel.scrollBy({
+        left: -scrollAmount,
+        behavior: 'smooth' // Zorgt voor vloeiend scrollen
+    });
+}
